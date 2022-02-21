00131e95e876c4245f7076124925fcba2413c3a6
LucasMotiv <mcmotiv@seznam.cz>
2022-02-21 21:17:00 +0100

    Responzivita (Úvod, Portfolio), přidání menu button

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
 $(document).ready(function(){
     $('.slider').slick({
-        centerMode: true,
         focusOnSelect: true,
         prevArrow: $('.arrows__left'),
         nextArrow: $('.arrows__right')
     });
 })
 
-$(".header__menu").click(function() {
-    $(".header__menu").toggleClass("active");
-    $(".header__navbar").toggleClass("active");
-    });
+$(document).ready(function () {
     
+    $(".cross").hide();
+    $(".menu").hide();
+    $(".hamburger").click(function () {
+      $(".menu").slideToggle("slow", function () {
+        $(".hamburger").hide();
+        $(".cross").show();
+      });
+    });
+  
+    $(".cross").click(function () {
+      $(".menu").slideToggle("slow", function () {
+        $(".cross").hide();
+        $(".hamburger").show();
+      });
+    });
+  
+  });
